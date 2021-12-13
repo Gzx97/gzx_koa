540bf65b82e5da4020bcd430a4dfcfff795e6a90
gzxdell <wanan499gzx@163.com>
2021-12-13 17:40:12 +0800

    appport

--- src/config/config.default.js
+++ src/config/config.default.js
@@ -3,7 +3,10 @@ const dotenv = require("dotenv");
 // console.log(process.env);
 dotenv.config();
 
-console.log(process.env)
+// console.log(process.env)
+console.log('---------start------')
+console.log(process.env.App_PORT)
 console.log(process.env.APP_PORT)
+console.log('---------end-------')
 
 module.exports = process.env;
