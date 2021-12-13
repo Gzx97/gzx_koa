d6705aee63c422dc7b40c05a857f8a119ef22d40
gzxdell <wanan499gzx@163.com>
2021-12-13 17:05:03 +0800

    dk

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ const app = require('./app')
 // app.listen(APP_PORT, () => {
 //   console.log(`server is running on http://localhost:${APP_PORT}`);
 // });
-console.log(APP_PORT)
+console.log('端口：'+APP_PORT)
+
 app.listen(8000, () => {
   console.log(`server is running on http://localhost:${8000}`);
 });
