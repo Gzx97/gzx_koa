12d6f903e4553cddca9c50124f4823d0b3833a38
gzxdell <wanan499gzx@163.com>
2021-12-17 15:43:18 +0800

    错误处理

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,4 @@
-const { createUser } = require("../service/user.service");
+const { createUser ,getUserInfo } = require("../service/user.service");
 class UserController {
   async register(ctx, next) {
     // 获取数据 操作数据库(service层) 返回结果
@@ -6,11 +6,38 @@ class UserController {
 
     const { user_name, password } = ctx.request.body;
 
+    // console.log(res);
+    // 合法性 
+    if (!user_name || !password) {
+      ctx.status = 400;
+      ctx.body = {
+        code: "10001",
+        msg: "用户名密码为空",
+        data: "",
+      };
+      return;
+    }
+    // 合理性
+
+     if(getUserInfo({user_name})){
+      ctx.status = 409;
+      ctx.body = {
+        code: "10002",
+        msg: "用户已经存在",
+        data: "",
+      };
+      return
+     }
     const res = await createUser(user_name, password);
 
-    // console.log(res);
-
-    ctx.body = ctx.request.body;
+    ctx.body = {
+      code: 0,
+      msg: "注册成功",
+      data: {
+        id: res.id,
+        user_name: res.user_name,
+      },
+    };
   }
 
   async login(ctx, next) {
